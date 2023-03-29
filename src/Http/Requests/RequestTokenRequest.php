b2519392964621e58eee2404f9cfd1c15a542b66
SanderGo <sander@yumb.nl>
2023-03-29 10:50:18 +0200

    Updates requestToken validation to accept email and phone as well

--- src/Http/Requests/RequestTokenRequest.php
+++ src/Http/Requests/RequestTokenRequest.php
@@ -15,8 +15,25 @@ class RequestTokenRequest extends FormRequest
     {
         return [
             'user_identifier' => 'required|string',
+            'email' => 'sometimes|email',
+            'phone' => 'sometimes|string',
             'user_id_type' => 'sometimes|string',
             'intended_url' => 'sometimes|string',
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        if(!isset($this->user_identifier) && isset($this->email))
+            $this->merge([
+                'user_identifier' => $this->email,
+                'user_id_type' => 'email'
+            ]);
+
+        if(!isset($this->user_identifier) && isset($this->phone))
+            $this->merge([
+                'user_identifier' => $this->phone,
+                'user_id_type' => 'sms'
+            ]);
     }
 }
