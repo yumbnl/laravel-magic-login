6a6f2e7e22426ebc89f6d3e8d9a5bd9a726d38c9
SanderGo <SanderGo@users.noreply.github.com>
2023-03-29 15:18:16 +0000

    Fix styling

--- tests/MagicLoginTest.php
+++ tests/MagicLoginTest.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Event;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertIsString;
+use function PHPUnit\Framework\assertTrue;
 use Yumb\MagicLogin\Events\TokenRequestedEvent;
 use Yumb\MagicLogin\Exceptions\ExpiredTokenException;
 use Yumb\MagicLogin\Facades\MagicLogin;
 use Yumb\MagicLogin\Tests\TestModels\User;
-
-use function PHPUnit\Framework\assertEquals;
-use function PHPUnit\Framework\assertIsString;
-use function PHPUnit\Framework\assertTrue;
 
 it('can create a login token for a user with given email', function () {
     $email = fake()->email();
